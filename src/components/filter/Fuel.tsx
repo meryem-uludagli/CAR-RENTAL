f04882b1e85f83bcb0cc3239e6b43febb3ba7f32
meryem-uludagli <meryemuslu2035@gmail.com>
2025-02-04 14:55:34 +0200

    add

--- src/components/filter/Fuel.tsx
+++ src/components/filter/Fuel.tsx
@@ -1,7 +1,17 @@
 import { FC } from "react";
 
 const Fuel: FC = () => {
-  return <div>Fuel</div>;
+  return (
+    <form className="flex flex-col">
+      <label>Fuel:</label>
+
+      <input
+        type="string"
+        className="w-24 py-[6px] px-2 rounded-[4px] shadow text-black bg-white"
+        placeholder="gasoline"
+      />
+    </form>
+  );
 };
 
 export default Fuel;
--- src/components/filter/Year.tsx
+++ src/components/filter/Year.tsx
@@ -1,7 +1,34 @@
-import { FC } from "react";
+import { param } from "motion/react-client";
+import { FC, useState, FormEvent } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Year: FC = () => {
-  return <div>Year</div>;
+  const [year, setYear] = useState<string>("");
+  const [params, setParams] = useSearchParams();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (year.trim()) {
+      params.set("year", year);
+    } else {
+      params.delete("year");
+    }
+    setParams(params);
+  };
+  return (
+    <form className="flex flex-col" onSubmit={handleSubmit}>
+      <label>Year:</label>
+
+      <input
+        type="number"
+        className="w-24 py-[6px] px-2 rounded-[4px] shadow text-black bg-white"
+        placeholder="Ex:2024"
+        onChange={(e) => setYear(e.target.value)}
+        defaultValue={params.get("year") as string}
+      />
+    </form>
+  );
 };
 
 export default Year;
